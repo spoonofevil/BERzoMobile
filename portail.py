6baed3da1d85df7594d922f93dd7defd6e4b49d9
spoonofevil <tanguydg@hotmail.com>
2023-01-15 17:04:25 +0100

    tout marche avec RNG et ecoute d'appel en boucle infini

--- portail.py
+++ portail.py
@@ -27,6 +27,7 @@ pathRessource="D:"
 
 def printUntilString(string):
     while True:#attente infini
+        print("on attend "+string)
         rep = getLineInString()
         print(rep)
         if (string in rep):
@@ -38,53 +39,64 @@ def printUntilString(string):
 #met le bon mode PDU pour envoyer des SMS
 def initSMS():
     modem.send_command(CMGF.write_cmd(1))
-    print(getLineInString())
-    pass
+    #print(getLineInString())
+    #modem.read_response()
+    printUntilString("CMGF")
 
 def lsDirectory():
     modem.send_command(FSCD.encoded_bytes())
-
     print(getLineInString())
 
 def initRessource(path):
     modem.send_command(FSCD.encoded_bytes())
-    print(getLineInString())
+    #print(getLineInString())
+    printUntilString("FSCD")
 
 
 #s'authentifie pour pouvoir utiliser GPRS
 def initAuthTCP():
     modem.send_command(CSOCKAUTH.write_cmd([1,0]))
-    print(getLineInString())
+    #print(getLineInString())
+    #modem.read_response()
+    printUntilString("CSOCKAUTH")
 
 #passe en mode UMTS only
 def modeUMTS():
     modem.send_command(CNMP.write_cmd(14))
-    print(getLineInString())
-
+    #print(getLineInString())
+    printUntilString("CNMP")
 #établi une connexion avec l'APN : gateway de l'internet
 def initAPN():
     modem.send_command(CGSOCKCONT.write_cmd([1,"IP","mmsbouygtel.com"]))
-    print(getLineInString())
+    #print(getLineInString())
+    printUntilString("CGSOCKCONT")
 
-urlOfRandomNumber="https://www.calculator.net"
+
+urlOfRandomNumber="calculator.net"
 getRequest="/random-number-generator.html?slower=0&supper=1&ctype=1&s=3860&submit1=Generate"
 startOfAnswer="<h2 class=\"h2result\">Result</h2><p class=\"verybigtext\" style=\"word-wrap: break-word;padding: 10px 0px;\">"
 def randomNumberOfHTTPGet():
     print("on envoie la request")
-    modem.send_command(CHTTPACT.write_cmd([urlOfRandomNumber, 443]))
+    modem.send_command(CHTTPACT.write_cmd([urlOfRandomNumber, 80]))
     print("command sent")
     printUntilString("CHTTPACT: REQUEST")
     modem.send_data(b'GET /random-number-generator.html?slower=0&supper=1&ctype=1&s=3860&submit1=Generate \r\n\r\n')
     modem.send_data(eof)
     print("on a envoyer la requete")
     reponse = [getLineInString()]
-    while ("CHTTPACT: 0" not in reponse[-1]) and (startOfAnswer not in reponse[-1]):
-        print(reponse[-1])
+    while ("CHTTPACT: 0" not in reponse[-1]):
+        #print(reponse[-1])
+        if("h2result" in reponse[-1]):
+            print("trouvée "+reponse[-1])
+            resu=reponse[-1].split('break-word;padding: 10px 0px;">')[1][:1]
+            print("reponse "+resu)
+            modem._ser.flush()
+            return resu
         reponse.append(getLineInString())
-    if (startOfAnswer not in reponse[-1]):
+    if (startOfAnswer in reponse[-1]):
         return reponse[-1]
     else:
-        return None
+        return reponse
 
 
 #requete HTTP de l'url passé en param
@@ -101,11 +113,12 @@ def getHTTP(url):
     return reponse
 
 
-def sendSMStoNum(num):
+def sendSMStoNum(num,message):
     initSMS()
     modem.send_command(CMGS.write_cmd(num))
     time.sleep(1)
-    modem.send_data(b'message'+eof)
+    modem.send_data(bytes(message,"utf-8"))
+    modem.send_data(eof)
     print("sent")
     print(getLineInString())
     print("fini")
@@ -126,20 +139,20 @@ def getGoogle():
 #montre qui appelle après ring
 def enableCallerIDVisible():
     modem.send_command(CLIP.write_cmd(1))
-    print(getLineInString())
+    #print(getLineInString())
+    printUntilString("CLIP")
 
 
 #tout est dans le nom
 def answer():
     modem.send_command(ATA.write_cmd())
-    print(getLineInString())
+    printUntilString("ATA")
 
 
 def waitForCallAndGetNumber():
     print("mise en attente")
     printUntilString("RING")
-    ignore = getLineInString()
-    print(ignore)
+    str(modem._ser.readline())
     rep = str(modem._ser.readline())[2:-1]
     #répond et raccroche après avoir eu l'info du numéro
     answer()
@@ -149,7 +162,8 @@ def waitForCallAndGetNumber():
     return rep[len("+CLIP: \""):len("+CLIP: \"") + 12]
 
 def getLineInString():
-    return modem._ser.readline().decode().strip("\\n")#.strip("\\r")[2:-1]
+    rep=modem._ser.readline().decode().strip("\\n")#.strip("\\r")[2:-1]
+    return rep
 
 
 def enableAutoAnswer():
@@ -161,6 +175,7 @@ def hangup():
     printUntilString("CHUP")
 
 
+
 if __name__ == "__main__":
     with SerialModem(DEVICE_PORT, DEVICE_PIN) as modem:
         #test d'envoie reponse de AT
@@ -169,15 +184,23 @@ if __name__ == "__main__":
         print(f"URC received since last:\n{urc}")
         print(f"Response:\n{response}")
         time.sleep(1)
-
         #playSoundAtPath(pathRessource,"oof.amr") #marche pas :c
         #enableAutoAnswer()
+        modeUMTS()
+        initAPN()
+        initAuthTCP()
         enableCallerIDVisible()
-        numberCalling=waitForCallAndGetNumber()
-        print(numberCalling+" is calling !")
-        answerRandom=randomNumberOfHTTPGet()
-        if (answerRandom == None):
-            print("pas de reponse")
-        elif answerRandom.removesuffix(startOfAnswer)[:1]:
-            print(answerRandom)
-    print("fini")
+        while(True):
+            numberCalling=waitForCallAndGetNumber()
+            print(numberCalling+" is calling !")
+            answerRandom=randomNumberOfHTTPGet()
+            answerSMS=""
+            if (answerRandom == None):
+                answerSMS="site down"
+            if(answerRandom=="1"):
+                answerSMS="le portail s'ouvre\nbienvenue"
+            elif(answerRandom=="0"):
+                answerSMS="access denied"
+            if(answerSMS!=""):
+                sendSMStoNum(numberCalling,answerSMS)
+            print("reponse a envoyer "+answerSMS)
